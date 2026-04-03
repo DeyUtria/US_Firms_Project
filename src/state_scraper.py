357fc4186349798f15d37c77e521783adbe0770b
DeyUtria <manuelparra3412@gmail.com>
2026-04-02 20:03:16 -0400

    Added libraries for executable and timestamp on file name. Trying to implement user input code

--- src/state_scraper.py
+++ src/state_scraper.py
@@ -6,9 +6,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from datetime import datetime
+import os
+import sys
 import pandas as pd
 
-# Set the driver path, website path and headless mode using msedge. Disabling GPU mode for Windows
+# Set the driver path, website path and headless mode using msedge. Disabling GPU mode for Windows. Adding path for executable file.
 website = "https://www.us-barassociation.org/content/state-list/"
 path = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedgedriver.exe"
 service = Service(executable_path=path)
@@ -17,6 +20,9 @@ options.add_argument("--headless=new")
 options.add_argument("--disable-gpu")
 driver = webdriver.Edge(service=service, options=options)
 driver.get(website)
+app_path = os.path.dirname(sys.executable)
+now = datetime.now()
+strf = now.strftime("%m%d%Y")
 
 # Variables for the first and second pages (States list and firm names with their US Bar Association website link)
 state_list = []
@@ -44,7 +50,8 @@ addresses_broken = []
 practice_areas_broken = []
 
 # Variable to select what states to scrape firms from
-target_states = ['Wyoming']
+print("If choosing multiple states, separate each other using a comma. (e.g: Alabama, California)")
+target_states = input("What state(s) do you want the firm list from? ")
 
 # Defining the first page xpath and looping through it to get all the states list. Also, make sure to click on the state link to access the firm list from said state.
 first_page_containers = driver.find_elements(by="xpath", value="//ul[@id='states-list']/li")
@@ -161,29 +168,10 @@ df_main_no_website['Practice Area'] = df_main_no_website['Practice Area'].mask(d
 
 # Save the DataFrame to a CSV file
 # df_main.to_csv("D:/Git/US_Firms_Project/data/firms_list.csv", index=False)
-df_main_no_website.to_csv("D:/Git/US_Firms_Project/data/firms_list_no_website.csv", index=False)
+df_main_no_website.to_csv(f"D:/Git/US_Firms_Project/data/Firms-List-{strf}.csv", index=False)
 # df_broken.to_csv("D:/Git/US_Firms_Project/data/firms_list_broken.csv", index=False)
 # df_practice_areas.to_csv("D:/Git/US_Firms_Project/data/practice_areas.csv", index=False)
 # df_practice_areas_broken.to_csv("D:/Git/US_Firms_Project/data/practice_areas_broken.csv", index=False)
-
+print("!Program executed successfully!")
 # Quitting the program
 driver.quit()
-
-#second_page_containers = driver.find_elements(by="xpath", value="//div[@class='inner']")
-#third_page_containers = driver.find_elements(by="xpath", value="//section[@class='firm-detail-content']")
-#third_page_containers_broken = driver.find_elements(by="xpath", value="//div[@id='apus-main-content']")
-                                
-#2nd page xpaths
-#//div[@class='inner']/div[@class='job-title-wrapper']/h2/a.get_attribute("href") details button xpath
-
-#3rd page xpaths
-#//section[@class='firm-detail-content']/div[@class='detail-phones']/p firm's phone number xpath "works on both good layouts but not the broken one"
-#//section[@class='firm-detail-content']/div/p[@class='detail-description'] firm's description xpath "works on both good layouts but not the broken one"
-#//section[@class='firm-detail-content']/div/div[@class='col-sm-8 col-sm-pull-4 address-block']/p[@class='address-text'] firm's address xpath "works on both good layouts but not the broken one"
-#//section[@class='firm-detail-content']/div/div[@class='col-sm-8 col-sm-pull-4 address-block']/a firm's website xpath "works on first good layout but not the second good layout or the broken one"
-#//div[@id='apus-main-content']/section[@class='wrapper-main-page container inner']/div[@class='row']/div[@id='main-content']/section[@class='practice-areas-block']/ul/li[@class='main-area'] firm's practice areas xpath "works on both good layouts but not the broken one"
-
-#//div[@id='apus-main-content']/section[@class='wrapper-main-page container inner']/div[@class='row']/div[@class='detail-phones']/p firm's phone number xpath on the third page BROKENPAGE
-#//div[@id='apus-main-content']/section[@class='wrapper-main-page container inner']/div[@class='row']/div/p[@class='detail-description'] firm's description xpath on the third page BROKENPAGE
-#//div[@id='apus-main-content']/section[@class='wrapper-main-page container inner']/div[@class='row']/div[@id='main-content']/section[@class='firm-detail-block']/section[@class='firm-detail-content']/div[@class='col-sm-8 col-sm-pull-4 address-block']/p[@class='address-text'] firm's address xpath on the third page BROKENPAGE
-#//div[@id='apus-main-content']/section[@class='practice-areas-block']/ul/li[@class='main-area'] firm's practice areas xpath on the third page BROKENPAGE
